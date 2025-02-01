06c13a42040cc05d292d9c01ae2b7833460fa522
hulgee209 <batkhulege@gmail.com>
2025-02-01 21:28:15 +0800

    lab2

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
@@ -1,15 +1,25 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Rectangle implements Shape {
-    public double height;
-    public double width;
-    
-    public Rectangle(double height, double width){
-        this.height = height;
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
         this.width = width;
+        this.height = height;
     }
 
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
     public double getArea() {
         return height * width;
     }
+
 }
